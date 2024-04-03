4e4a7e1012dc3398dec0ec1ab2f000505eb7f8f0
mvrzan <mvrzan@icloud.com>
2024-04-03 14:59:44 -0400

    updates to the home page

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,8 +1,45 @@
+import { Box } from "@twilio-paste/box";
+import { Stack } from "@twilio-paste/core";
+import { Flex } from "@twilio-paste/core/flex";
+import { Text } from "@twilio-paste/core/text";
+
+import homeBanner from "../../assets/page-content/home_banner.png";
+
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <Box
+      width="100%"
+      height="20vh"
+      backgroundColor="colorBackgroundPrimaryWeak"
+    >
+      <Flex
+        hAlignContent="between"
+        vAlignContent="center"
+        marginLeft="space80"
+        marginRight="space80"
+      >
+        <Stack orientation="vertical" spacing="space50">
+          <Text
+            as="p"
+            textAlign={"left"}
+            fontSize="fontSize80"
+            fontWeight="fontWeightBold"
+            verticalAlign="center"
+          >
+            Member Exclusive
+          </Text>
+          <Text as="p" textAlign={"left"} fontSize="fontSize40">
+            15% off everything + extra $10 off for the first order!
+          </Text>
+        </Stack>
+        <img
+          src={homeBanner}
+          alt="two people posing"
+          draggable={false}
+          height="210"
+        />
+      </Flex>
+    </Box>
   );
 };
 
