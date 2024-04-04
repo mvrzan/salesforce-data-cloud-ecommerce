18b5c19bfcafd6e22fd105d5408f764706e63add
mvrzan <mvrzan@icloud.com>
2024-04-04 11:15:37 -0400

    add product component to the home page

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -2,48 +2,77 @@ import { Box } from "@twilio-paste/box";
 import { Stack } from "@twilio-paste/core";
 import { Flex } from "@twilio-paste/core/flex";
 import { Text } from "@twilio-paste/core/text";
+import { Grid, Column } from "@twilio-paste/core/grid";
 
-import homeBanner from "../../assets/page-content/home_banner.png";
+import Product from "../UI/Product";
 import useBearStore from "../hooks/useBearStore";
+import homeBanner from "../../assets/page-content/home_banner.png";
+
+interface ProductProps {
+  category: string;
+  description: string;
+  id: number;
+  image: string;
+  price: number;
+  rating: { rate: number; count: number };
+  title: string;
+}
 
 const Home = () => {
   const products = useBearStore((state) => state.products);
-  console.log(products);
 
   return (
-    <Box
-      width="100%"
-      height="20vh"
-      backgroundColor="colorBackgroundPrimaryWeak"
-    >
-      <Flex
-        hAlignContent="between"
-        vAlignContent="center"
-        marginLeft="space80"
-        marginRight="space80"
+    <>
+      <Box
+        width="100%"
+        height="20vh"
+        backgroundColor="colorBackgroundPrimaryWeak"
       >
-        <Stack orientation="vertical" spacing="space50">
-          <Text
-            as="p"
-            textAlign={"left"}
-            fontSize="fontSize80"
-            fontWeight="fontWeightBold"
-            verticalAlign="center"
-          >
-            Member Exclusive
-          </Text>
-          <Text as="p" textAlign={"left"} fontSize="fontSize40">
-            15% off everything + extra $10 off for the first order!
-          </Text>
-        </Stack>
-        <img
-          src={homeBanner}
-          alt="two people posing"
-          draggable={false}
-          height="210"
-        />
-      </Flex>
-    </Box>
+        <Flex
+          hAlignContent="between"
+          vAlignContent="center"
+          marginLeft="space80"
+          marginRight="space80"
+        >
+          <Stack orientation="vertical" spacing="space50">
+            <Text
+              as="p"
+              textAlign={"left"}
+              fontSize="fontSize80"
+              fontWeight="fontWeightBold"
+              verticalAlign="center"
+            >
+              Member Exclusive
+            </Text>
+            <Text as="p" textAlign={"left"} fontSize="fontSize40">
+              15% off everything + extra $10 off for the first order!
+            </Text>
+          </Stack>
+          <img
+            src={homeBanner}
+            alt="two people posing"
+            draggable={false}
+            height="210"
+          />
+        </Flex>
+      </Box>
+      <Grid marginTop="space60">
+        {products?.map((product: ProductProps) => {
+          return (
+            <Grid gutter="space40" key={product.id}>
+              <Column>
+                <Product
+                  key={product.id}
+                  title={product.title}
+                  image={product.image}
+                  price={product.price}
+                />
+              </Column>
+            </Grid>
+          );
+        })}
+      </Grid>
+    </>
   );
 };
 
