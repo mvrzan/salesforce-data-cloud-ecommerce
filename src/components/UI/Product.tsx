ce91b136b0d8291363ab5562787ac83c38a4a29e
mvrzan <mvrzan@icloud.com>
2024-04-04 16:15:04 -0400

    expand the product component for the modal usage

--- src/components/UI/Product.tsx
+++ src/components/UI/Product.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { Box } from "@twilio-paste/box";
 import { Flex } from "@twilio-paste/core/flex";
 import { Text } from "@twilio-paste/core/text";
@@ -6,59 +8,93 @@ import { Button } from "@twilio-paste/core/button";
 import { Truncate } from "@twilio-paste/core/truncate";
 import { Separator } from "@twilio-paste/core/separator";
 
+import ProductDetailsModal from "./ProductDetailsModal";
+
 interface ProductProps {
   image: string;
   price: number;
   title: string;
+  description: string;
+  rating: { rate: number; count: number };
 }
 
-const Product = ({ image, price, title }: ProductProps) => {
+const Product = ({
+  image,
+  price,
+  title,
+  description,
+  rating,
+}: ProductProps) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
-    <Box
-      borderRadius="borderRadius30"
-      boxShadow="shadow"
-      margin="space40"
-      width="500px"
-      height="500px"
-    >
-      <Flex vAlignContent="center" hAlignContent="center">
-        <Box marginTop="space40" marginBottom="space40">
-          <img
-            src={image}
-            alt="two people posing"
-            draggable={false}
-            height="210"
-          />
-        </Box>
-      </Flex>
-      <Separator orientation="horizontal" verticalSpacing="space30" />
-      <Stack orientation="vertical" spacing="space200">
-        <Text
-          as="h1"
-          fontSize="fontSize40"
-          fontWeight="fontWeightBold"
-          textAlign="center"
-          margin="space40"
-        >
-          <Truncate title="product title">{title}</Truncate>
-        </Text>
-        <Text
-          as="h1"
-          fontSize="fontSize70"
-          fontWeight="fontWeightBold"
-          textAlign="center"
-          color="colorTextLink"
-        >
-          ${price}
-        </Text>
-      </Stack>
-      <Flex vAlignContent="center" hAlignContent="center" marginTop="space60">
-        <Stack orientation="horizontal" spacing="space80">
-          <Button variant="primary">Add to Cart</Button>
-          <Button variant="secondary">View Details</Button>
+    <>
+      {isModalOpen && (
+        <ProductDetailsModal
+          setIsModalOpen={setIsModalOpen}
+          image={image}
+          price={price}
+          title={title}
+          description={description}
+          rating={rating}
+        />
+      )}
+      <Box
+        borderRadius="borderRadius30"
+        boxShadow="shadow"
+        margin="space40"
+        width="500px"
+        height="500px"
+      >
+        <Flex vAlignContent="center" hAlignContent="center">
+          <Box marginTop="space40" marginBottom="space40">
+            <img
+              src={image}
+              alt="two people posing"
+              draggable={false}
+              height="210"
+            />
+          </Box>
+        </Flex>
+        <Separator orientation="horizontal" verticalSpacing="space30" />
+        <Stack orientation="vertical" spacing="space200">
+          <Text
+            as="h1"
+            fontSize="fontSize40"
+            fontWeight="fontWeightBold"
+            textAlign="center"
+            margin="space40"
+          >
+            <Truncate title="product title">{title}</Truncate>
+          </Text>
+          <Text
+            as="h1"
+            fontSize="fontSize70"
+            fontWeight="fontWeightBold"
+            textAlign="center"
+            color="colorTextLink"
+          >
+            ${price}
+          </Text>
         </Stack>
-      </Flex>
-    </Box>
+        <Flex vAlignContent="center" hAlignContent="center" marginTop="space60">
+          <Stack orientation="horizontal" spacing="space80">
+            <Button
+              element="BUTTON_STATIC_POSITION"
+              variant="secondary"
+              onClick={() => {
+                setIsModalOpen(!isModalOpen);
+              }}
+            >
+              View Details
+            </Button>
+            <Button element="BUTTON_STATIC_POSITION" variant="primary">
+              Add to Cart
+            </Button>
+          </Stack>
+        </Flex>
+      </Box>
+    </>
   );
 };
 
