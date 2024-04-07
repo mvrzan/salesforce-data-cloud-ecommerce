aa3abdb9ee5567fadecdad2015d79bbf1d4abacb
mvrzan <mvrzan@icloud.com>
2024-04-06 20:27:35 -0400

    update state store to handle cart logic

--- src/components/hooks/useBearStore.ts
+++ src/components/hooks/useBearStore.ts
@@ -1,13 +1,31 @@
 import { create } from "zustand";
 import { fetchProductData } from "../../utils/fetchData";
 
+interface Product {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+}
+
 type BearStore = {
   products: [];
   fetch: () => Promise<void>;
+  cart: Product[];
+  addToCart: (product: Product) => void;
+  numberOfItemsInCart: number;
+  clearCart: () => void;
 };
 
 const useBearStore = create<BearStore>((set) => ({
+  cart: [],
   products: [],
+  numberOfItemsInCart: 0,
   fetch: async () => {
     try {
       const data = await fetchProductData();
@@ -15,6 +33,14 @@ const useBearStore = create<BearStore>((set) => ({
     } catch (error) {
       console.error("Error setting global state:", error);
     }
+  },
+  addToCart: (product: Product) => {
+    set((state) => ({ cart: [...state.cart, product] }));
+    set((state) => ({ numberOfItemsInCart: state.numberOfItemsInCart + 1 }));
+  },
+  clearCart: () => {
+    set(() => ({ numberOfItemsInCart: 0 }));
+    set(() => ({ cart: [] }));
   },
 }));
 
