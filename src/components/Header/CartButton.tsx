6824ee7046982580340e48786d577a104bac2d4c
mvrzan <mvrzan@icloud.com>
2024-04-06 20:28:16 -0400

    enhance the cart button with cart item counter

--- src/components/Header/CartButton.tsx
+++ src/components/Header/CartButton.tsx
@@ -1,10 +1,31 @@
+import { useNavigate } from "react-router-dom";
+
 import { TiShoppingCart } from "react-icons/ti";
 import { Button } from "@twilio-paste/core/button";
+import { Badge } from "@twilio-paste/core/badge";
+import useBearStore from "../hooks/useBearStore";
+import { Stack } from "@twilio-paste/core";
 
 const CartButton = () => {
+  const navigate = useNavigate();
+  const numberOfItemsInCart = useBearStore(
+    (state) => state.numberOfItemsInCart
+  );
+
   return (
-    <Button variant="primary_icon" size="reset">
-      <TiShoppingCart size="40" />
+    <Button
+      variant="primary_icon"
+      size="reset"
+      onClick={() => {
+        navigate("/cart");
+      }}
+    >
+      <Stack orientation="horizontal" spacing="space40">
+        <TiShoppingCart size="40" />
+        <Badge as="span" variant="neutral_counter">
+          {numberOfItemsInCart >= 99 ? "99+" : numberOfItemsInCart}
+        </Badge>
+      </Stack>
     </Button>
   );
 };
