50fe2128648c4d4f35ee8ce692fdaa9698a84088
mvrzan <mvrzan@icloud.com>
2025-08-28 09:13:35 -0400

    add Data Cloud web sdk type to the window interface

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -8,5 +8,118 @@ export interface Product {
     rate: number;
     count: number;
   };
-  quantity?: number;
+  quantity?: number | undefined;
+}
+
+declare global {
+  export interface Window {
+    SalesforceInteractions: {
+      init: (config: { consents: { provider: string; purpose: string; status: string }[] }) => Promise<void>;
+
+      ConsentPurpose: { Tracking: string };
+      ConsentStatus: { OptIn: string };
+      getAnonymousId: () => string;
+
+      CatalogObjectInteractionName: {
+        ViewCatalogObject: string;
+      };
+
+      CartInteractionName: {
+        AddToCart: string;
+        RemoveFromCart: string;
+      };
+
+      OrderInteractionName: {
+        Purchase: string;
+      };
+
+      sendEvent: (
+        event:
+          | {
+              interaction: {
+                name: string;
+                chatMessage?: string;
+                eventType?: string;
+                category?: string;
+
+                catalogObject?: {
+                  type: string;
+                  id: string;
+                  attributes: {
+                    name?: string;
+                    category?: string;
+                    price?: number;
+                    rating?: number;
+                    productDescription?: string;
+                    productName?: string;
+                  };
+                };
+
+                lineItem?: {
+                  catalogObjectType: string;
+                  catalogObjectId: string;
+                  quantity: number | undefined;
+                  price: number;
+                  currency: string;
+                  attributes: {
+                    title: string;
+                    description: string;
+                    rating: number;
+                    image: string;
+                  };
+                };
+
+                order?: {
+                  id: string;
+                  totalValue: number;
+                  currency: string;
+                  lineItems: {
+                    catalogObjectType: string;
+                    catalogObjectId: string;
+                    quantity: number | undefined;
+                    price: number;
+                    currency: string;
+                    attributes: {
+                      title: string;
+                      description: string;
+                      rating: number;
+                      image: string;
+                    };
+                  }[];
+                };
+
+                firstName?: string;
+                lastName?: string;
+                email?: string;
+                phoneNumber?: string;
+                isAnonymous?: string;
+              };
+              user?: {
+                attributes: {
+                  name?: string;
+                  eventType: string;
+                  firstName: string;
+                  lastName: string;
+                  isAnonymous: string;
+                  email: string;
+                  phoneNumber: string;
+                };
+              };
+            }
+          | {
+              user: {
+                attributes: {
+                  name?: string;
+                  eventType: string;
+                  firstName: string;
+                  lastName: string;
+                  isAnonymous: string;
+                  email: string;
+                  phoneNumber: string;
+                };
+              };
+            }
+      ) => void;
+    };
+  }
 }
