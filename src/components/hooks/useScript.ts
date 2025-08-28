27db4cbc9d603e8c991ed1d558a4a85fe58ef46d
mvrzan <mvrzan@icloud.com>
2025-08-28 09:14:38 -0400

    rewrite to async await

--- src/components/hooks/useScript.ts
+++ src/components/hooks/useScript.ts
@@ -1,32 +1,26 @@
 import { writeToLocalStorage } from "../../utils/localStorageUtil";
 
-declare const window: Window &
-  typeof globalThis & {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    SalesforceInteractions: any;
-  };
-
 const useScript = () => {
   const configureScriptUrl = (url: string) => {
     const script = document.createElement("script");
     script.src = url;
 
-    script.onload = () => {
-      window.SalesforceInteractions.init({
-        consents: [
-          {
-            provider: "Test Provider",
-            purpose: window.SalesforceInteractions.ConsentPurpose.Tracking,
-            status: window.SalesforceInteractions.ConsentStatus.OptIn,
-          },
-        ],
-      })
-        .then(() => {
-          console.log("Salesforce Interactions initialized");
-        })
-        .catch((error: Error) => {
-          console.error("Salesforce Interactions failed to initialize", error);
+    script.onload = async () => {
+      try {
+        await window.SalesforceInteractions.init({
+          consents: [
+            {
+              provider: "Test Provider",
+              purpose: window.SalesforceInteractions.ConsentPurpose.Tracking,
+              status: window.SalesforceInteractions.ConsentStatus.OptIn,
+            },
+          ],
         });
+
+        console.log("Salesforce Interactions initialized");
+      } catch (error) {
+        console.error("Salesforce Interactions failed to initialize", error);
+      }
     };
 
     document.head.appendChild(script);
